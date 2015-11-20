bb4b4b9949e8c80f6e40e6efbb394c1abfa3d123
Legokichi Duckscallion <legokichi@gmail.com>
2015-11-20 17:48:01 +0900

    tmp

--- app/src/main/java/com/duxca/app/MainActivity.java
+++ app/src/main/java/com/duxca/app/MainActivity.java
@@ -1,72 +1,40 @@
 package com.duxca.app;
 
-import java.io.*;
+import android.bluetooth.*;
+import android.content.*;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.hardware.usb.*;
-import com.hoho.android.usbserial.driver.*;
-import android.content.*;
-import android.util.Log;
+import android.widget.TextView;
+import java.io.*;
 
 public class MainActivity extends AppCompatActivity {
-    UsbManager manager;
-    UsbSerialDriver usb;
+    BluetoothAdapter Bt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        manager = (UsbManager) getSystemService(Context.USB_SERVICE);
-        usb = UsbSerialProber.acquire(manager);
+        BluetoothAdapter Bt = BluetoothAdapter.getDefaultAdapter();
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        boolean btEnable = Bt.isEnabled();
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, usb != null ? "usb detected." :"usb not detected.", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-                if (usb != null) {
-                    try{
-                        usb.open();
-                        usb.setBaudRate(9600);
-                        start_read_thread(); // シリアル通信を読むスレッドを起動
-                    }
-                    catch(IOException e){
-                        e.printStackTrace();
-                    }
-                }
-
+                Snackbar.make(view, "usb detected.", Snackbar.LENGTH_LONG).setAction("Action", null).show();
             }
         });
-    }
-
-    public void start_read_thread(){
-        new Thread(new Runnable(){
-            public void run(){
-                Log.v("usb serial thread", "thread running.");
-                try{
-                    while(true){
-                        byte buf[] = new byte[256];
-                        int num = usb.read(buf, buf.length);
-                        if(num > 0) Log.v("usb arduino", new String(buf, 0, num)); // Arduinoから受信した値をlogcat出力
-                        Thread.sleep(10);
-                    }
-                }catch(IOException e){
-                    e.printStackTrace();
-                }catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }).start();
     }
 
     @Override
